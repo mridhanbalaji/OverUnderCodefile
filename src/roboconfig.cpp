03df22b9380e28655236a168ea11be3e00bf09f7
mridhanbalaji <mridhanbalaji210706@gmail.com>
2023-12-05 23:54:51 -0500

    PTO Drive

--- src/roboconfig.cpp
+++ src/roboconfig.cpp
@@ -4,12 +4,13 @@
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 
-pros::Motor left_front_motor(11,pros::E_MOTOR_GEAR_600, true); // port 2, not reversed
-pros::Motor left_back_motor(1,pros::E_MOTOR_GEAR_600, false); // port 1, not reversed
-pros::Motor left_PTO_motor(12,pros::E_MOTOR_GEAR_600, false); // port 2, not reversed
-pros::Motor right_front_motor(19,pros::E_MOTOR_GEAR_600, true); // port 3, reversed
+pros::Motor left_front_motor(1,pros::E_MOTOR_GEAR_600, true); // port 2, not reversed
+pros::Motor left_back_motor(2,pros::E_MOTOR_GEAR_600, false); // port 1, not reversed
+pros::Motor left_PTO_motor(3,pros::E_MOTOR_GEAR_600, false); // port 2, not reversed
+pros::Motor right_front_motor(8,pros::E_MOTOR_GEAR_600, true); // port 3, reversed
 pros::Motor right_back_motor(9,pros::E_MOTOR_GEAR_600, false); // port 4, reversed.
-pros::Motor right_PTO_motor(7,pros::E_MOTOR_GEAR_600, false); // port 4, reversed.
+pros::Motor right_PTO_motor(10
+,pros::E_MOTOR_GEAR_600, false); // port 4, reversed.
 
  
 // drivetrain motor groups
